6ee75c9739b4d9acfa4e2aeb915c2d1f177bbac9
efraimmgon <efraimmaugusto@gmail.com>
2018-10-05 17:56:36 -0400

    write pprint-middleware

--- src/sublimerepl/core.clj
+++ src/sublimerepl/core.clj
@@ -1,6 +1,18 @@
 (ns sublimerepl.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn pprint-middleware 
+  "pprint in Clojure SublimeREPL.
+  
+  In your project.clj add it to :repl-option:
+  
+  (defproject sample \"0.1.0\"
+    :dependencies [[org.clojure/clojure \"1.4.0\"]
+                   [sublimerepl \"X.X.X\"]]
+    :repl-options {:nrepl-middleware [sublimerepl/pprint-middleware]})"
+  [handler]
+  (fn [{:keys [code op] :as msg}]
+    (if (and (= op "eval") (not (empty? code)))
+      (->> #(str "(clojure.pprint/pprint " % ")")
+           (update-in msg [:code])
+           handler)
+      (handler msg))))
